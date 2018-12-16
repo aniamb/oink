0763d64e7389a4affa97ff736bc915a9cb1a4b4d
Anita <aambalav@purdue.edu>
2018-12-15 22:36:49 -0500

    Update oink.py
    handles words

--- oink.py
+++ oink.py
@@ -1,2 +1,11 @@
-input = raw_input("Enter a string: ")
+input = raw_input("Enter a word: ")
 type(input)
+
+first = input[1:]
+
+second = input[0:1]
+
+third = "ay"
+
+pigLatin = first+second+third
+print "Pig Latin Translation: " + pigLatin

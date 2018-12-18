f127451d1bf80ec7fcbd2e5d2d1cab0b294641d5
Anita <aambalav@purdue.edu>
2018-12-18 08:32:37 -0500

    start while loop

--- oink.py
+++ oink.py
@@ -1,33 +1,51 @@
+continuation = 1
+while (continuation == 1):
+    choice = input("For English to Pig Latin, Enter 1. For Pig Latin to English, Press 2: \n")
+    type(choice)
 
-choice = input("For English to Pig Latin, Enter 1. For Pig Latin to English, Press 2: \n")
-type(choice)
+    if choice == 1:
+        input = raw_input("Enter English: ")
+        type(input)
 
-if choice == 1:
-    input = raw_input("Enter English: ")
-    type(input)
+        words = input.split(" ")
 
-    words = input.split(" ")
+        print "Pig Latin Translation: "
 
-    print "Pig Latin Translation: "
+        for x in words:
+            first = x[1:]
+            second = x[0:1]
+            third = "ay"
 
-    for x in words:
-        first = x[1:]
-        second = x[0:1]
-        third = "ay"
+            pigLatin = first+second+third
 
-        pigLatin = first+second+third
+            print pigLatin
 
-        print pigLatin
-elif choice == 2:
-    pInput = raw_input("Enter Pig Latin: ")
-    type(pInput)
+        keepGoing = raw_input("Would you like to translate another word? Enter Y or N: \n")
+        type(keepGoing)
 
-    print "Pig Latin Translation: "
-    pWords = pInput.split(" ")
-    for y in pWords:
-        sansAY = y[:-2]
-        firstLetter = sansAY[-1:]
-        secondHalf = sansAY[:-1]
-        print firstLetter+secondHalf
-else:
-    print("Invalid input")
+        if keepGoing == "Y":
+            continuation = 1
+        else:
+            continuation = 2
+
+    elif choice == 2:
+        pInput = raw_input("Enter Pig Latin: ")
+        type(pInput)
+
+        print "Pig Latin Translation: "
+        pWords = pInput.split(" ")
+        for y in pWords:
+            sansAY = y[:-2]
+            firstLetter = sansAY[-1:]
+            secondHalf = sansAY[:-1]
+            print firstLetter+secondHalf
+
+        keepGoing = raw_input("Would you like to translate another word? Enter Y or N: \n")
+        type(keepGoing)
+
+        if keepGoing == "Y":
+            continuation = 1
+        else:
+            continuation = 2
+    else:
+        print("Invalid input")
